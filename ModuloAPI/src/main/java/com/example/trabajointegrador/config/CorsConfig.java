6a1a63c468ea688b54098f7e29be62fe30d17f64
JuaniiEtche <juanietcheverry8@gmail.com>
2023-06-27 21:14:56 -0300

    Feature: cors2

--- ModuloAPI/src/main/java/com/example/trabajointegrador/config/CorsConfig.java
+++ ModuloAPI/src/main/java/com/example/trabajointegrador/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:19006","http://localhost:4200", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
