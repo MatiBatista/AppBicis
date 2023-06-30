692b7a5950c5f9c77d75cf9b652ab0bac51d0994
JuaniiEtche <juanietcheverry8@gmail.com>
2023-06-30 14:33:34 -0300

    Fix: error 202 si envia un soporte que no existe

--- ModuloAPI/src/main/java/com/example/trabajointegrador/controllers/SoportBiclycleController.java
+++ ModuloAPI/src/main/java/com/example/trabajointegrador/controllers/SoportBiclycleController.java
@@ -5,6 +5,7 @@ import com.example.trabajointegrador.entities.SoportBiclycle;
 import com.example.trabajointegrador.service.ISoportBiclycleService;
 import jakarta.persistence.GeneratedValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,9 +24,17 @@ public class SoportBiclycleController {
         soportService.delete(soportBiclycle);
     }
 
-    @GetMapping("/user/soport/{nombreSoporte}")
-    public boolean getSoporteHabilitado(@PathVariable String nombreSoporte){
-        return soportService.getSoporteHabilitado(nombreSoporte);
+    @GetMapping("/public/soport/{nombreSoporte}")
+    public ResponseEntity<?> getSoporteHabilitado(@PathVariable String nombreSoporte) {
+        boolean soporteHabilitado = soportService.getSoporteHabilitado(nombreSoporte);
+
+        if (soporteHabilitado) {
+            Boolean recurso = true;
+
+            return ResponseEntity.ok(recurso);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @PutMapping("/user/suport/{nombreSoporte}")
