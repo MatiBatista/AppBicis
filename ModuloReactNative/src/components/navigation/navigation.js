4faae8670936ad1e6b3666d2825807f00977f106
MatiBatista <matiasomarbatista1@gmail.com>
2023-07-02 19:31:48 -0300

    A

--- ModuloReactNative/src/components/navigation/navigation.js
+++ ModuloReactNative/src/components/navigation/navigation.js
@@ -66,7 +66,6 @@ export default function Navigation(){
         <Stack.Navigator screenOptions={{ headerShown: false, /* contentStyle: {backgroundColor: '#1C819F'}  */}}>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
-<<<<<<< HEAD
           <Stack.Screen name="Home" component={MyTabs} />
           <Stack.Screen name="Activity" component={Activity} />
           <Stack.Screen name="Notification" component={Notification} />
@@ -77,33 +76,5 @@ export default function Navigation(){
       </NavigationContainer>
     )
 }
-=======
-          </>
-        ) : (
-          <>
-            {role === 'admin' && (
-              <>
-              <Stack.Screen name="HomeAdmin" component={HomeAdmin} />
-              <Stack.Screen name="SoportesBicicletas" component={SoportesBicicletas} />
-              <Stack.Screen name="AltaSoporteBici" component={AltaSoporteBici} />
-              <Stack.Screen name="VisualizarSoporteBici" component={VisualizarSoporteBici} />
-              <Stack.Screen name="ModalVisualizarSoporteBici" component={MyModal} />
-              </>
-            )}
-            {role === 'user' && (
-              <>
-                <Stack.Screen name="Home" component={MyTabs} />
-                <Stack.Screen name="Activity" component={Activity} />
-                <Stack.Screen name="ScanQR" component={ScanQR} />
-                <Stack.Screen name="Notificacion" component={Notification} />
-              </>
-            )}
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
->>>>>>> 64a2b52e9451c8c0cb8e51557b7203a4fc996e10
 
   
